fc49c67c6131b0c22a7cf055583ca94a75fb84a0
614259037 <614259037@webmail.npru.ac.th>
2020-11-08 14:04:40 +0700

    first

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv').config();
+const Unsplash = require('unsplash-js').default;
+const {toJson} = require('unsplash-js');
+const unsplash = new 
+Unsplash({accessKey:process.env.myAccessKey});
+//3. Configuration here
+
+
+//4. add API here
+router.post('/api/searchPhotos', (req,res)=>{
+let {keyword} = req.body;
+    unsplash.search.photos(keyword,1,21)
+    .then(toJson)
+    .then(json =>{
+       res.status(200).send(json)
+    })
+    .catch((err)=>{
+        console.log("Error Message -->", err);
+        res.status(200).send({"messageError":err})
+    });
+});
+
+
+router.post('/api/searchUser', (req, res) => {
+    let {
+        username
+    } = req.body;
+    unsplash.users.profile(username)
+        .then(toJson)
+        .then(json => {
+            res.status(200).send(json)
+        })
+        .catch((err) => {
+            console.log("Error Message -->", err);
+            res.status(200).send({
+                "messageError": err
+            })
+        });
+});
+router.post('/api/userphoto', (req, res) => {
+    let {
+        username
+    } = req.body;
+    unsplash.users.photos(username, 1, 12, "latest")
+        .then(toJson)
+        .then(json => {
+            res.status(200).send(json)
+        })
+        .catch((err) => {
+            console.log("Error Message -->", err);
+            res.status(200).send({
+                "messageError": err
+            })
+        });
+});
+module.exports = router;
